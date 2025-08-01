a7df4f22339b12f512f68c3f6f39790264dc8d16
AMIR990704 <amir.h.moridi@gmail.com>
2025-08-01 11:55:22 +0330

    Initial commit

---
+++ src/main/java/com/example/bank_commission_service/BankCommissionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.bank_commission_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankCommissionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankCommissionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bank_commission_service/BankCommissionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bank_commission_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankCommissionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

b6b72b2eb9d9f594cddfb927d1b9c18dcf3ca6fa
AMIR990704 <amir.h.moridi@gmail.com>
2025-08-01 15:01:50 +0330

    Add repository package

---
+++ src/main/java/com/example/bank_commission_service/repository/user/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.example.bank_commission_service.repository.user;
+
+import com.example.bank_commission_service.model.user.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(String name);
+}
---
+++ src/main/java/com/example/bank_commission_service/repository/user/UserRepository.java
@@ -0,0 +1,15 @@
+package com.example.bank_commission_service.repository.user;
+
+import com.example.bank_commission_service.model.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    boolean existsByUsername(String username);
+}
